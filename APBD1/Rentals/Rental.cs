0b867f6371f53cf67aaae816c533c604e1be1454
Katarzyna Rak <rakkatarzyna83@gmail.com>
2026-03-22 18:36:12 +0100

    Add class for rental

---
+++ APBD1/Rentals/Rental.cs
@@ -0,0 +1,31 @@
+﻿using APBD1.Utils;
+
+namespace APBD1.Rentals;
+
+public class Rental
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int EquipmentId { get; set; }
+
+    public DateTime RentDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+
+    public float LateFee { get; set; }
+
+    public Rental(int userId, int equipmentId, DateTime rentDate, int days)
+    {
+        Id = IdGenerator.GenerateRentalId();
+        UserId = userId;
+        EquipmentId = equipmentId;
+        RentDate = rentDate;
+        DueDate = rentDate.AddDays(days);
+    }
+
+    public void Return(DateTime returnDate, float lateFee)
+    {
+        ReturnDate = returnDate;
+        LateFee = lateFee;
+    }
+}
--- APBD1/Utils/IdGenerator.cs
+++ APBD1/Utils/IdGenerator.cs
@@ -4,6 +4,7 @@ public class IdGenerator
 {
     private static int _equipmentId = 1;
     private static int _userId = 1;
+    private static int _rentalId = 1;
 
     public static int GenerateEquipmentId()
     {
@@ -12,5 +13,9 @@ public class IdGenerator
     public static int GenerateUserId()
     {
         return _userId++;
+    }
+    public static int GenerateRentalId()
+    {
+        return _rentalId++;
     }
 }
